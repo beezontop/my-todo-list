6eb6518de6df3607f0b6077c0cc69a93c0aafeb7
Doris <d1882002@gmail.com>
2020-08-13 15:29:58 +0800

    Updates

---
+++ precache-manifest.e3eb8f45b6e6233b9f2d070486e8c6c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bb7882410b94abc57e448590ef29fbd",
+    "url": "/my-todo-list/index.html"
+  },
+  {
+    "revision": "8fbe5331e3b64b303566",
+    "url": "/my-todo-list/static/css/main.62a76d08.chunk.css"
+  },
+  {
+    "revision": "de10588d4301b9eab409",
+    "url": "/my-todo-list/static/js/2.2cb26e01.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-todo-list/static/js/2.2cb26e01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fbe5331e3b64b303566",
+    "url": "/my-todo-list/static/js/main.15c4743a.chunk.js"
+  },
+  {
+    "revision": "30e3b95480c8544da2ef",
+    "url": "/my-todo-list/static/js/runtime-main.18a084b2.js"
+  }
+]);
